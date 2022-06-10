27fc8eed6298bb06fbbc2e260f7651ee1984d580
Caroliny S Q G Cagnoni <cgcagnoni@gmail.com>
2022-06-10 17:51:05 -0300

    Adicionando resolução do exercício 1.1. com for

--- exercicio01/Program.cs
+++ exercicio01/Program.cs
@@ -1,1 +1,18 @@
-﻿teste
+﻿// Essa é a lista de filmes disponibilizada pelo professor inicialmente.
+String[] listaDeFilmes = { "Clube dos Cinco", "A Hora do Pesadelo", "Karatê Kid", "Rocky IV", "Conan, o Bárbaro", "Highlander" };
+
+// 1.1. Apresenta todos os filmes da lista separados por ponto e vírgula.
+string apresentaTodosOsFilmesSeparadosPorPontoVirgula()
+{
+    string lista = ""; 
+    for (int indice = 0; indice < listaDeFilmes.Length; indice++)
+    {
+        //lista = listaDeFilmes[indice] + ";"; -> assim só pega o último item (sobreposição)
+        //lista = lista + listaDeFilmes[indice] + ";"; -> faz o mesmo que a linha de baixo
+        lista += $"{listaDeFilmes[indice]}; ";
+    }
+    return lista;
+}
+
+Console.WriteLine(apresentaTodosOsFilmesSeparadosPorPontoVirgula());
+
